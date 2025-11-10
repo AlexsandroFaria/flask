27145fcbd824f37f7771ea01e24331a8673a9715
AlexsandroFaria <alexsandro.lfaria@gmail.com>
2025-11-10 17:49:51 -0300

    New alteracions

--- estudo/views.py
+++ estudo/views.py
@@ -1,6 +1,17 @@
 from estudo import app
-from flask import render_template
+from flask import render_template, url_for
 
 @app.route('/')
 def homepage():
-    return render_template("index.html")
+    usuario = 'Alexsandro'
+    idade = 34
+
+    context = {
+        'usuario': 'Alex',
+        'idade': 45
+    }
+    return render_template("index.html", context=context)
+
+@app.route('/nova/')
+def novapagina():
+    return "Outra view"
